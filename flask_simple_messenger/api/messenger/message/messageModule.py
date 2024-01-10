01179e20147fc132398e8fe3ce662cd8a3d826ff
Stanislav Kutuzov <stanislavkutyzov@gmail.com>
2024-01-10 17:44:11 +0300

    feat Implement message module - implement messege sending and recieving features

--- flask_simple_messenger/api/messenger/message/messageModule.py
+++ flask_simple_messenger/api/messenger/message/messageModule.py
@@ -1,9 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
+
+from api.auth.user.userModel import User
+from api.auth.session.sessionModel import Session
+from api.messenger.message.messageModel import Message
+from app import App
+from api.auth.authModule import validateToken
+
+db = App.db()
 
 def getMessages():
-    return 'list of messages'
+    response, user = validateToken()
+    if not(user):
+        return response
+    contactID = request.form['contact']
+    contact = User.query.filter_by(id=contactID).first()
+    outcommingMessagesQuery = Message.query.filter_by(reciever_id=contact.id, sender_id=user.id)
+    incommingMessagesQuery = Message.query.filter_by(reciever_id=user.id, sender_id=contact.id)
+    messageList = incommingMessagesQuery.union(outcommingMessagesQuery).all()
+    messageList = [{'id':message.id, 
+                    'sender':message.sender.username, 
+                    'reciever':message.reciever.username, 
+                    'message':message.message,
+                    } for message in messageList]
+    response = jsonify(messageList)
+    return response
 
 def postMessage():
+    response, user = validateToken()
+    if not(user):
+        return response
+    contactID = request.form['contact']
+    contact = User.query.filter_by(id=contactID).first()
+    if not(contact):
+        return 'not valid reciever'
+    messageBody = request.form['message']
+    if not(messageBody):
+        return 'message is empty'
+    message = Message(sender_id=user.id, reciever_id=contact.id, message=messageBody)
+    db.session.add(message)
+    db.session.commit()
     return 'message posted'
 
 class MessagesModule(Blueprint):
